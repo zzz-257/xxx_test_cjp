01b7eb944ed3556c898bc9e665875adae81fbb2d
chenjianping <chenjp@nlscan.com>
2022-10-20 03:03:59 +0000

    sencond add +4

---
+++ 4.c
@@ -0,0 +1,20 @@
+/**********************************************************************************************
+* COPYRIGHT NOTICE
+* Copyright (c) 2020
+* All rights reserved
+* @author :chenjp
+* @file :/home/chenjianping/tmp/xxx_test_cjp\4.c
+* @date :2022/10/20 03:19
+* @description :
+**********************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc,char *argv[]){
+
+    return 0;
+}
+
---
+++ 5.c
@@ -0,0 +1,20 @@
+/**********************************************************************************************
+* COPYRIGHT NOTICE
+* Copyright (c) 2020
+* All rights reserved
+* @author :chenjp
+* @file :/home/chenjianping/tmp/xxx_test_cjp\5.c
+* @date :2022/10/20 03:22
+* @description :
+**********************************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+int main(int argc,char *argv[]){
+
+    return 0;
+}
+
